3853d492b9b5b58295743f052bd7a028d3df57af
atiqa zafar <atiqazafarr@gmail.com>
2018-11-17 15:37:14 +0100

    Add AGM

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppComponent } from './app.component';
 import { DashboardHelperComponent } from './components/dashboard-helper/dashboard-helper.component';
 import { DashboardNeedyComponent } from './components/dashboard-needy/dashboard-needy.component';
 import { AppRoutingModule } from './app-routing.module';
+import { MapComponent } from './components/map/map.component';
+import { AgmCoreModule} from '@agm/core';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardHelperComponent,
-    DashboardNeedyComponent
+    DashboardNeedyComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAMvIYdGx_M_RF4zc2FyirOegr7284xcvc'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
